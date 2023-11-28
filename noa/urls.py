d4512970557d8187a4c45e50089a2ed31c665965
Ynifa <yasine.nifa@gmail.com>
2023-11-28 08:49:58 +0100

    Add list template
    Signed-off-by: Ynifa <yasine.nifa@gmail.com>

--- noa/urls.py
+++ noa/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path("report/", include('report.urls')),
+    path("compte/", include('compte.urls')),
 ]
